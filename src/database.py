a3abbd96f31f7acd50dfc35e06cd044d9601fa55
Kevin Gamez <ks.gamez@uniandes.edu.co>
2024-03-12 10:04:13 -0500

    fix docker compose

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:password@db/db"
+DATABASE_URL = "postgresql://postgres:password@db:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 
