7a56a555d62571335c47be48d112c9c555f16a3b
Kevin Gamez <49690792+kevingamez@users.noreply.github.com>
2024-03-12 21:02:34 -0500

    changes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 from typing import List, Annotated
 import enum
-import models
-from database import sessionLocal, engine
-from sqlalchemy.orm import Session
-from sqlalchemy.sql.expression import func
-import uvicorn
 from recomendation_system import get_song_recommendations
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+)
 models.Base.metadata.create_all(bind=engine)
 
 def get_db():
